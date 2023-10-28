7f7e2243b9565174a1cb0f7d1d1d1c5f92feb648
Derek Lim <dereklim3006@gmail.com>
2023-10-28 13:25:49 -0400

    Create homepage and add style.

---
+++ src/home.js
@@ -0,0 +1,83 @@
+export default function homePage() {
+    const content = document.querySelector('#content');
+
+    //Add restaurant name
+    const name = document.createElement('div');
+    name.classList.add('name');
+    name.classList.add('one');
+    name.textContent = "Beary's Breakfast Bar";
+    content.append(name);
+
+    //Add description section
+    const description = document.createElement('div');
+    description.classList.add('description');
+    description.classList.add('two');
+    content.append(description);
+    //Add text for description
+    const descriptionText = document.createElement('p');
+    descriptionText.classList.add('text');
+    descriptionText.textContent = "Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again."
+    description.append(descriptionText);
+    //Add author for text
+    const descriptionAuthor = document.createElement('div');
+    descriptionAuthor.classList.add('author');
+    descriptionAuthor.textContent = 'Goldilocks'
+    description.append(descriptionAuthor);
+
+    //Add store hours section
+    const hours = document.createElement('div');
+    hours.classList.add('hours');
+    hours.classList.add('three');
+    content.append(hours);
+    //Add heading for store hours
+    const hoursHeading = document.createElement('div');
+    hoursHeading.classList.add('heading');
+    hoursHeading.textContent = 'Hours';
+    hours.append(hoursHeading);
+    //Add Sunday hours
+    const hoursSunday = document.createElement('div');
+    hoursSunday.textContent = 'Sunday: 8am - 8pm';
+    hours.append(hoursSunday);
+    //Add Monday hours
+    const hoursMonday = document.createElement('div');
+    hoursMonday.textContent = 'Monday: 6am - 6pm';
+    hours.append(hoursMonday);
+    //Add Tuesday hours
+    const hoursTuesday = document.createElement('div');
+    hoursTuesday.textContent = 'Tuesday: 6am - 6pm';
+    hours.append(hoursTuesday);
+    //Add Wednesday hours
+    const hoursWednesday = document.createElement('div');
+    hoursWednesday.textContent = 'Wednesday: 6am - 6pm';
+    hours.append(hoursWednesday);
+    //Add Thursday hours
+    const hoursThursday = document.createElement('div');
+    hoursThursday.textContent = 'Thursday: 6am - 10pm';
+    hours.append(hoursThursday);
+    //Add Friday hours
+    const hoursFriday = document.createElement('div');
+    hoursFriday.textContent = 'Friday: 6am - 10pm';
+    hours.append(hoursFriday);
+    //Add Saturday hours
+    const hoursSaturday = document.createElement('div');
+    hoursSaturday.textContent = 'Saturday: 8am - 10pm';
+    hours.append(hoursSaturday);
+
+    //Add Location section
+    const location = document.createElement('div');
+    location.classList.add('location');
+    location.classList.add('four');
+    content.append(location);
+    //Add heading for location section
+    const locationHeading = document.createElement('div');
+    locationHeading.classList.add('heading');
+    locationHeading.textContent = 'Location';
+    location.append(locationHeading);
+    //Add address
+    const address = document.createElement('div')
+    address.classList.add('address');
+    address.textContent = '123 Forest Drive, Forestville, Maine';
+    location.append(address);
+ 
+    return content;
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,68 @@
-import _ from 'lodash';
 import './style.css';
+import homePage from './home.js';
+
+document.body.append(header());
+document.body.append(homePage());
+document.body.append(footer());
+
+function header() {
+    const header = document.createElement('header');
+    //Add nav
+    const nav = document.createElement('nav');
+    header.append(nav);
+    //Add "home" button
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('home');
+    homeButton.textContent = 'Home';
+    nav.append(homeButton);
+    //Add "menu" button
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('menu');
+    menuButton.textContent = 'Menu';
+    nav.append(menuButton);
+    //Add "contact" button
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('contact');
+    contactButton.textContent = 'Contact';
+    nav.append(contactButton);
+
+    return header;
+}
+
+function footer() {
+    const footer = document.createElement('footer');
+    //Add link to original website
+    const a = document.createElement('a');
+    a.href = 'https://eckben.github.io/bearysBreakfastBar/';
+    a.textContent = 'Real website - https://eckben.github.io/bearysBreakfastBar/';
+    footer.append(a);
+    //Add link to background image
+    const a1 = document.createElement('a');
+    a1.href = 'https://www.freepik.com/vectors/food';
+    a1.textContent = 'Background image created by dgim-studio - www.freepik.com';
+    footer.append(a1);
+
+    return footer;
+}
+
+const homeButton = document.querySelector('.home');
+
+homeButton.addEventListener('click', () => {
+    //remove everything inside div#content
+    const one = document.querySelector('.one');
+    one.remove();
+    const two = document.querySelector('.two');
+    two.remove();
+    const three = document.querySelector('.three');
+    three.remove();
+    const four = document.querySelector('.four');
+    four.remove();
+    //insert "home" content into div#content
+    document.body.append(homePage());
+
+    //remove footer
+    const previousFooter = document.querySelector('footer');
+    previousFooter.remove();
+    //create footer
+    document.body.append(footer());
+});
